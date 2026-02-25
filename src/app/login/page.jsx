0ee19e6ec7b3804a81fbbf0acbf378b74db38f11
asifemran890 <freelancerasif890@gmail.com>
2026-02-25 23:14:37 +0600

    added

--- src/app/login/page.jsx
+++ src/app/login/page.jsx
@@ -3,7 +3,7 @@ import React from "react";
 const login = () => {
   return (
     <div>
-      <h1>This is a login pages helloa  </h1>
+      <h1>This is a login pages hello plese login now </h1>
     </div>
   );
 };
